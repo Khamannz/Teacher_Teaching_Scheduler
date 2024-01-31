7ae8064cfbc053b57c2d327eb913dabe498e3a90
Kha Mann <Khamann@gmail.com>
2024-01-31 22:46:15 +0700

    first commit

---
+++ src/Utils.java
@@ -0,0 +1,12 @@
+public class Utils {
+    public void displayMenu() {
+        System.out.println("==== Teacher Scheduler ====");
+        System.out.println("1. Add Teacher");
+        System.out.println("2. Add Subject");
+        System.out.println("3. Assign Subject to Teacher");
+        System.out.println("4. Set Teacher Availability");
+        System.out.println("5. Generate Schedules");
+        System.out.println("6. View Schedules");
+        System.out.println("7. Exit");
+    }
+}
